3e361c86154f974ab05d867b87ba2887836198bc
ei1111 <34093023+ei1111@users.noreply.github.com>
2023-11-30 15:11:48 +0900

    Update WebConfig.java

--- src/main/java/notice/pratice/config/WebConfig.java
+++ src/main/java/notice/pratice/config/WebConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
- /*   @Override
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(customInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/user/save", "/auth/login", "/error","/swagger-resources/**","/swagger-ui.html","/v2/api-docs","/webjars/**" , "/" , "/csrf");
-    }*/
+    }
 
     @Bean
     public JoinCheckInterceptor customInterceptor() {
